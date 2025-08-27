597d7b6ca04bb505f22edff617e938223a9962cd
Alexandre Matos <matosalexandre795@gmail.com>
2025-08-27 20:38:39 -0300

    Project Created!

---
+++ src/main/java/com/devsuperior/desafioEventos/DesafioEventosApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafioEventos/DesafioEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
