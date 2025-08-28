ca68ff02a5e25cc6937c886a215e40bebc18c1e8
Alexandre Matos <matosalexandre795@gmail.com>
2025-08-27 22:28:29 -0300

    New Class: Categoria & @ManytoOne(Atividade)

--- src/main/java/com/devsuperior/desafioEventos/entities/Categoria.java
+++ src/main/java/com/devsuperior/desafioEventos/entities/Categoria.java
@@ -3,81 +3,48 @@ package com.devsuperior.desafioEventos.entities;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
+
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_atividade")
-public class Atividade {
+@Table(name = "tb_categoria")
+public class Categoria {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String nome;
-
-    @Column(columnDefinition = "TEXT")
     private String descricao;
-    private Double preco;
 
-    @ManyToMany
-    @JoinTable(name = "tb_participante_atividade",
-        joinColumns = @JoinColumn(name = "atividade_id"),
-        inverseJoinColumns = @JoinColumn(name = "participante_id"))
-    private Set<Participante> participantes = new HashSet<>();
+    @OneToMany(mappedBy = "categoria")
+    private Set<Atividade> atividades = new HashSet<>();
 
-    @ManyToOne
-    @JoinColumn(name = "categoria_id")
-    private Categoria categoria;
-
-    public Atividade() {
+    public Categoria() {
     }
 
-    public Atividade(Integer id, String nome, String descricao, Double preco) {
+    public Categoria(Integer id, String descricao) {
         this.id = id;
-        this.nome = nome;
         this.descricao = descricao;
-        this.preco = preco;
     }
 
     public Integer getId() {
         return id;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
     public String getDescricao() {
         return descricao;
-    }
-
-    public Double getPreco() {
-        return preco;
     }
 
     public void setId(Integer id) {
         this.id = id;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public void setDescricao(String descricao) {
         this.descricao = descricao;
-    }
-
-    public void setPreco(Double preco) {
-        this.preco = preco;
     }
 
     @Override
@@ -96,7 +63,7 @@ public class Atividade {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Atividade other = (Atividade) obj;
+        Categoria other = (Categoria) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
